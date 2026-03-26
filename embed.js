c20d2301cd669e94efed8ca78c3067bc8bd06f38
cyno-benzene <55052924+cyno-benzene@users.noreply.github.com>
2026-03-26 22:53:11 +0530

    updated workflow to ensure dist/ is deployed

--- embed.js
+++ embed.js
@@ -1,4 +1,8 @@
 (function() {
+  // Get the base URL of where this script is hosted
+  const scriptUrl = new URL(document.currentScript.src);
+  const baseUrl = scriptUrl.origin + scriptUrl.pathname.substring(0, scriptUrl.pathname.lastIndexOf('/'));
+  
   // Create container
   const container = document.createElement('div');
   container.id = 'interstellar-clock-container';
@@ -15,9 +19,9 @@
   const threeScript = document.createElement('script');
   threeScript.src = 'https://unpkg.com/three@v0.149.0/build/three.min.js';
   
-  // Load the clock script
+  // Load the clock script from the same directory as this embed script
   const clockScript = document.createElement('script');
-  clockScript.src = 'https://cyno-benzene.github.io/interstellar-clock/interstellar-clock.iife.js';
+  clockScript.src = baseUrl + '/interstellar-clock.iife.js';
   
   // Initialize after both load
   let loaded = 0;
@@ -27,6 +31,10 @@
       window.InterstellarClock.initInterstellarClock('interstellar-clock-container');
     }
   };
+  
+  // Add error handlers
+  threeScript.onerror = () => console.error('Failed to load Three.js');
+  clockScript.onerror = () => console.error('Failed to load Interstellar Clock');
   
   threeScript.onload = init;
   clockScript.onload = init;
